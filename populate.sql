569e8594ec224b945942b4aea1b5a00e7b12857b
Sviatoslav <55200530+sTorba24@users.noreply.github.com>
2021-12-13 20:23:23 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,35 +1,34 @@
 ---children
 INSERT INTO children (child_name)
-  	VALUES ('SAM');
+	VALUES ('SAM');
 INSERT INTO children (child_name)
   	VALUES ('JOHN');
 INSERT INTO children (child_name)
-  	VALUES ('IVAN');
+   	VALUES ('IVAN');
 INSERT INTO children (child_name)
-  	VALUES ('SVIAT');
+   	VALUES ('SVIAT');
 INSERT INTO children (child_name)
   	VALUES ('VASYL');
-    
 ---humans
 INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (24,'MYKOLA','YES','MALE',1);
+ 	VALUES (24,'MYKOLA','YES','MALE',12);
 INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (27,'ANDRIY','NO','MALE',2);
+   	VALUES (27,'ANDRIY','NO','MALE',11);
 INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (19,'VLADYSLAV','NO','MALE',3);
+  	VALUES (19,'VLADYSLAV','NO','MALE',14);
 INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (22,'KEVIN','YES','MALE',5);
+  	VALUES (22,'KEVIN','YES','MALE',13);
 INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (16,'STELLA','NO','MALE',4);
-    
+  	VALUES (16,'STELLA','NO','MALE',15);
+ 
 ---insuranse 
-INSERT INTO insuranse (charge,hum_id)
-  	VALUES (1625,1);
-INSERT INTO insuranse (charge,hum_id)
-  	VALUES (2750,3);    
-INSERT INTO insuranse (charge,hum_id)
-  	VALUES (3000,5);    
-INSERT INTO insuranse (charge,hum_id)
-  	VALUES (1900,2);
-INSERT INTO insuranse (charge,hum_id)
-  	VALUES (1520,4);
+INSERT INTO insuranse (ins_charge,hum_id)
+ 	VALUES (1625,7);
+INSERT INTO insuranse (ins_charge,hum_id)
+   	VALUES (2750,9);    
+INSERT INTO insuranse (ins_charge,hum_id)
+  	VALUES (3000,11);    
+INSERT INTO insuranse (ins_charge,hum_id)
+ 	VALUES (1900,8);
+INSERT INTO insuranse (ins_charge,hum_id)
+   	VALUES (1520,10);
