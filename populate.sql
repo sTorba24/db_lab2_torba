f101547ce025b93a22cf13db5992dd2e745da006
Sviatoslav <55200530+sTorba24@users.noreply.github.com>
2021-12-27 16:28:43 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -10,25 +10,38 @@ INSERT INTO children (child_name)
 INSERT INTO children (child_name)
   	VALUES ('VASYL');
 ---humans
-INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
- 	VALUES (24,'MYKOLA','YES','MALE',12);
-INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-   	VALUES (27,'ANDRIY','NO','MALE',11);
-INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (19,'VLADYSLAV','NO','MALE',14);
-INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (22,'KEVIN','YES','MALE',13);
-INSERT INTO humans (hum_age,hum_name,hum_smoker,hum_sex,child_id)
-  	VALUES (16,'STELLA','NO','MALE',15);
+INSERT INTO humans (hum_age,hum_name,hum_sex,child_id)
+ 	VALUES (24,'MYKOLA','MALE',2);
+INSERT INTO humans (hum_age,hum_name,hum_sex,child_id)
+   	VALUES (27,'ANDRIY','MALE',3);
+INSERT INTO humans (hum_age,hum_name,hum_sex,child_id)
+  	VALUES (19,'VLADYSLAV','MALE',5);
+INSERT INTO humans (hum_age,hum_name,hum_sex,child_id)
+  	VALUES (22,'KEVIN','MALE',4);
+INSERT INTO humans (hum_age,hum_name,hum_sex,child_id)
+	VALUES (16,'STELLA','FEMALE',1);
+	
+--smokers
+
+INSERT INTO smokers (isSmoker,smoker_date,hum_id)
+ 	VALUES ('YES','2017-03-14',1);
+INSERT INTO smokers (isSmoker,hum_id)
+ 	VALUES ('NO',2);
+INSERT INTO smokers (isSmoker,smoker_date,hum_id)
+ 	VALUES ('YES','2018-04-11',4);
+INSERT INTO smokers (isSmoker,smoker_date,hum_id)
+ 	VALUES ('YES','2019-02-23',3);
+INSERT INTO smokers (isSmoker,hum_id)
+ 	VALUES ('NO',5);
  
 ---insuranse 
 INSERT INTO insuranse (ins_charge,hum_id)
- 	VALUES (1625,7);
+ 	VALUES (1625,3);
 INSERT INTO insuranse (ins_charge,hum_id)
-   	VALUES (2750,9);    
+   	VALUES (2750,2);    
 INSERT INTO insuranse (ins_charge,hum_id)
-  	VALUES (3000,11);    
+  	VALUES (3000,5);    
 INSERT INTO insuranse (ins_charge,hum_id)
- 	VALUES (1900,8);
+ 	VALUES (1900,1);
 INSERT INTO insuranse (ins_charge,hum_id)
-   	VALUES (1520,10);
+   	VALUES (1520,4);
